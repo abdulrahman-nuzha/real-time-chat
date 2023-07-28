c2a49d0b1c641a0592301f3355db28d55af36893
AbdulRahman Nuzha <abdalrahmannuzha@gmail.com>
2023-07-28 19:02:54 +0300

    created and added relations

---
+++ app/Models/Friend.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Friend extends Model
+{
+    use HasFactory;
+
+        /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'user_id',
+        'to_user_id',
+        'status',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [];
+    /**
+     * Friend Model Relations
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class,'user_id');
+    }
+}
