c2a6b5e96a9c114d0a409f2b7caacecbd8730e1a
AbdulRahman Nuzha <abdalrahmannuzha@gmail.com>
2023-07-28 19:04:44 +0300

    created and implement index function

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Support\Facades\Auth;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request)
+    {
+        $user = $request->user();
+        $users = [
+            [
+                'id' => '1',
+                'name' => 'John Doe',
+                'status' => 'offline',
+            ],
+            [
+                'id' => '2',
+                'name' => 'Jane Smith',
+                'status' => 'online',
+            ],
+            [
+                'id' => '3',
+                'name' => 'Smith Smith',
+                'status' => 'online',
+            ],
+        ];
+        //dd($users);
+        // $users = $user->with('friends');
+        // $users = $user->friends();
+        // dd(json_encode($users));
+        return view('dashboard')->with(['users' => $users]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
