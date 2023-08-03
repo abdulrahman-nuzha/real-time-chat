cba5b4aa8c941637ddce82d09f748a5423932f7b
AbdulRahman Nuzha <abdalrahmannuzha@gmail.com>
2023-08-03 03:01:26 +0300

    make dashboard route need verified user

--- routes/user.php
+++ routes/user.php
@@ -14,7 +14,7 @@ use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 
-Route::middleware('auth')->group(function () {
+Route::middleware(['auth','verified'])->group(function () {
 
     Route::get('/dashboard', [UserController::class, 'index'])->name('dashboard');
 });
