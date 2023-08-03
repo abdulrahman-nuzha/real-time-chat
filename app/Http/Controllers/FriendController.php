14ad1433b1e16f3975c4494b84aba9c7efb4913b
AbdulRahman Nuzha <abdalrahmannuzha@gmail.com>
2023-08-03 03:15:42 +0300

    define the index function and return list of users friends

--- app/Http/Controllers/FriendController.php
+++ app/Http/Controllers/FriendController.php
@@ -2,9 +2,79 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Friend;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class FriendController extends Controller
 {
-    //
+    /**
+     * Display a listing of user friends.
+     */
+    public function index(Request $request)
+    {
+        $friends = Friend::where('user_id',$request->user()->id)
+        //->where('status','approved')
+        ->with("to_user")
+        ->simplePaginate(10);
+
+        return view('friends.list', [
+            'friends' => $friends,
+        ]);
+    }
+
+    // /**
+    //  * Dispaly friend info
+    //  */
+    // public function friendProfile(){
+    //     return view('profile.partials.profile-card');
+    // }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
 }
