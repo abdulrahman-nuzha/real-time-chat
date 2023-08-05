246b9c90637a28502a9cd0be53ae0a0e402de600
AbdulRahman Nuzha <abdalrahmannuzha@gmail.com>
2023-08-05 23:53:08 +0300

    created : search bar livewire

---
+++ resources/views/search.blade.php
@@ -0,0 +1,8 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Chatty') }}
+        </h2>
+    </x-slot>
+    <livewire:search-bar />
+</x-app-layout>
