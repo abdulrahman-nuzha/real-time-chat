cd6d9049b190242f7f9f3a2d38b70b45956e76c0
AbdulRahman Nuzha <abdalrahmannuzha@gmail.com>
2023-08-05 23:47:36 +0300

    moved profile routes to user

--- routes/web.php
+++ routes/web.php
@@ -22,12 +22,6 @@ Route::get('/', function () {
 //     return view('dashboard');
 // })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware(['auth','verified'])->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
 require __DIR__.'/auth.php';
 require __DIR__.'/user.php';
 require __DIR__.'/friend.php';
