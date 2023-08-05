e426b67eafb07e4e6881b6bb08b3b708a0020fb4
AbdulRahman Nuzha <abdalrahmannuzha@gmail.com>
2023-08-05 23:48:19 +0300

    moved profie routes from web and added user prefix

--- routes/user.php
+++ routes/user.php
@@ -10,11 +10,19 @@ use App\Http\Controllers\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\VerifyEmailController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\ProfileController;
 use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 
-Route::middleware(['auth','verified'])->group(function () {
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::group(["prefix" => "user"], function () {
+        Route::get('/dashboard', [UserController::class, 'index'])->name('dashboard');
+        Route::get('/search', [UserController::class, 'search'])->name('search');
+        Route::get('/profile/{id}', [UserController::class, 'profile'])->name('user.profile');
 
-    Route::get('/dashboard', [UserController::class, 'index'])->name('dashboard');
+        Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+        Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+        Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    });
 });
