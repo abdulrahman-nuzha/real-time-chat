3c1a68297421cf37599fa28620740ad5085eb443
AbdulRahman Nuzha <abdalrahmannuzha@gmail.com>
2023-08-05 23:52:16 +0300

    removing profile card and added link to profile page

--- resources/views/friends/list.blade.php
+++ resources/views/friends/list.blade.php
@@ -8,7 +8,7 @@
     <div class="py-12">
         @if ($friends)
             @foreach ($friends as $friend)
-                <a href="#"
+                <a href="{{ route('user.profile', ['id' => $friend->to_user->id]) }}"
                     class="max-w-4xl mx-auto my-3 sm:px-6 lg:px-8 block hover:bg-gray-100 dark:hover:bg-gray-700 hover:shadow-sm rounded-lg profile-card-toggle-link"
                     data-user-id="{{ $friend->id }}">
                     <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
@@ -41,43 +41,13 @@
                         </div>
                     </div>
                 </a>
-
-                <div id="overlay"
-                    class="fixed top-0 left-0 w-full h-full z-50 bg-black bg-opacity-50 backdrop-filter blur-lg hidden">
-                </div>
-                {{-- <a href="#" class="profile-card-hide" data-user-id="{{ $friend->user_id }}">
-            <div id="profile-card-{{ $friend->user_id }}"
-                class="profile-card hidden fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-50 bg-white p-6 rounded-lg shadow-lg">
-                @include('profile.partials.profile-card')
-            </div>
-        </a> --}}
-
-                <div id="profile-card-{{ $friend->id }}"
-                    class="profile-card-close hidden fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-50 p-6 rounded-lg shadow-lg"
-                    data-user-id="{{ $friend->id }}">
-                    <button class="absolute top-0 right-0 m-8 text-gray-500 hover:text-gray-700 focus:outline-none">
-                        <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24"
-                            xmlns="http://www.w3.org/2000/svg">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                d="M6 18L18 6M6 6l12 12"></path>
-                        </svg>
-                    </button>
-                    
-                    @include('profile.partials.profile-card', [
-                    'name' => $friend->to_user->name,
-                    'username' => $friend->to_user->username,
-                    'profile_picture' => $friend->to_user->profile_picture,
-                    'status' => $friend->to_user->status,
-                    'email' => $friend->to_user->email,
-                ])
-                </div>
             @endforeach
         @endif
 
     </div>
 </x-app-layout>
 
---Scritps--
+{{-- --Scritps--
 <script>
     document.querySelectorAll('.profile-card-toggle-link').forEach(function(el) {
         el.addEventListener('click', function(event) {
@@ -97,4 +67,4 @@
             document.querySelector('#overlay').classList.toggle('hidden');
         });
     });
-</script>
+</script> --}}
