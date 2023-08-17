248d47e7c2e96c63bda4e058fb3f37cecbafdc12
AbdulRahman Nuzha <abdalrahmannuzha@gmail.com>
2023-08-18 02:28:33 +0300

    some improvments

--- resources/views/friends/list.blade.php
+++ resources/views/friends/list.blade.php
@@ -8,63 +8,40 @@
     <div class="py-12">
         @if ($friends)
             @foreach ($friends as $friend)
-                <a href="{{ route('user.profile', ['id' => $friend->to_user->id]) }}"
-                    class="max-w-4xl mx-auto my-3 sm:px-6 lg:px-8 block hover:bg-gray-100 dark:hover:bg-gray-700 hover:shadow-sm rounded-lg profile-card-toggle-link"
-                    data-user-id="{{ $friend->id }}">
-                    <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="max-w-4xl mx-auto my-3 sm:px-6 lg:px-8 rounded-lg shadow-xl">
+                    <div class="bg-white dark:bg-gray-800 ove rflow-hidden shadow-sm sm:rounded-lg">
                         <div class="p-6 text-gray-900 dark:text-gray-100">
                             <div class="flex justify-between items-center">
-                                <div class="flex items-center">
-                                    <div class="rounded-full overflow-hidden mr-2">
-                                        <img src="{{ asset($friend->to_user->profile_picture) }}" alt="Profile Picture"
-                                            class="h-8 w-8 object-cover">
+                                <a href="{{ route('user.profile', ['id' => $friend->id]) }}">
+                                    <div class="flex items-center">
+                                        <div class="rounded-full overflow-hidden mr-2">
+                                            <img src="{{ asset($friend->profile_picture) }}" alt="Profile Picture"
+                                                class="h-8 w-8 object-cover">
+                                        </div>
+                                        <div>
+                                            <span class="font-semibold">{{ $friend->name }}</span>
+                                        </div>
                                     </div>
-                                    <div>
-                                        <span class="font-semibold">{{ $friend->to_user->name }}</span>
-                                    </div>
-                                </div>
+                                </a>
                                 <div class="mx-6 flex items-center">
-                                    @if ($friend->to_user->status == 'online')
+                                    @if ($friend->status == 'online')
                                         <svg class="h-4 w-4 text-green-500" fill="currentColor" viewBox="0 0 20 20">
                                             <circle cx="10" cy="10" r="5" />
                                         </svg>
-                                    @endif
-
-                                    @if ($friend->to_user->status == 'offline')
+                                    @elseif ($friend->status == 'offline')
                                         <svg class="h-4 w-4 text-red-500" fill="currentColor" viewBox="0 0 20 20">
                                             <circle cx="10" cy="10" r="5" />
                                         </svg>
                                     @endif
-                                    <div class="mx-1">{{ $friend->to_user->status }}</div>
+                                    <div class="mx-1">{{ $friend->status }}</div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </a>
+                </div>
             @endforeach
+            {{ $friends->links() }}
         @endif
 
     </div>
 </x-app-layout>
-
-{{-- --Scritps--
-<script>
-    document.querySelectorAll('.profile-card-toggle-link').forEach(function(el) {
-        el.addEventListener('click', function(event) {
-            event.preventDefault();
-            var userId = this.getAttribute('data-user-id');
-            document.querySelector('#profile-card-' + userId).classList.remove('hidden');
-            document.querySelector('#overlay').classList.remove('hidden');
-        });
-    });
-
-    document.querySelectorAll('.profile-card-close').forEach(function(el) {
-        el.addEventListener('click', function(event) {
-            event.preventDefault();
-
-            var userId = this.getAttribute('data-user-id');
-            document.querySelector('#profile-card-' + userId).classList.add('hidden');
-            document.querySelector('#overlay').classList.toggle('hidden');
-        });
-    });
-</script> --}}
