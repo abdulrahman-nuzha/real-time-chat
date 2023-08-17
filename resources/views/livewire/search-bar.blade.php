38fbdc05974c6fb2bc9dd849496c336ecb14abf8
AbdulRahman Nuzha <abdalrahmannuzha@gmail.com>
2023-08-18 02:25:54 +0300

    some improvments

--- resources/views/livewire/search-bar.blade.php
+++ resources/views/livewire/search-bar.blade.php
@@ -1,7 +1,5 @@
-<div class="relative flex justify-center items-center mx-auto mt-8 md:w-1/2 lg:w-1/3">
-    <!-- Increased mt-8 for more space -->
-    <div class="absolute inset-y-0 left-0 flex items-center pl-4 pointer-events-none">
-        <!-- Increased pl-4 for more space -->
+<div class="relative flex justify-center items-center mx-auto mt-8 md:w-1/2 lg:w-1/3 hover:text-gray-900 dark:hover:text-white">
+    <div class="absolute inset-y-0 left-0 flex items-center pl-4 pointer-events-none ">
         <svg class="w-5 h-5 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
             fill="none" viewBox="0 0 20 20">
             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
@@ -12,7 +10,6 @@
         class="form-input block w-full p-4 pl-12 text-base text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
         placeholder="Search..." wire:model.debounce.500ms="query" />
     @empty($query)
-
     @else
         @if (!empty($users))
             <div class="absolute top-16 w-full">
@@ -26,7 +23,9 @@
                             </li>
                         @endforeach
                     </ul>
-                    {{ $users->links() }}
+                    <div class="px-4 py-3 text-gray-900 dark:text-white hover:bg-gray-300 dark:hover:bg-gray-500 cursor-pointer">
+                        {{ $users->links() }}
+                    </div>
                 </div>
             </div>
         @elseif(empty($users))
