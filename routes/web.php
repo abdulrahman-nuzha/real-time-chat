ecd093a3f4eeef57b3dca920ec6a0fdd2464b7a0
AbdulRahman Nuzha <abdalrahmannuzha@gmail.com>
2023-09-02 01:21:28 +0300

    new test route for testing the pusher notification

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Events\NewNotification;
 use App\Http\Controllers\ProfileController;
+use App\Models\Notification;
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,10 +21,17 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/test', function () {
+    $notification = Notification::first();
+    $us = auth()->user();
+    event(new NewNotification($notification, $us));
+});
+
+
 // Route::get('/dashboard', function () {
 //     return view('dashboard');
 // })->middleware(['auth', 'verified'])->name('dashboard');
 
-require __DIR__.'/auth.php';
-require __DIR__.'/user.php';
-require __DIR__.'/friend.php';
+require __DIR__ . '/auth.php';
+require __DIR__ . '/user.php';
+require __DIR__ . '/friend.php';
