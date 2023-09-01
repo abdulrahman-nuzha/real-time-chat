d40aafcc057ca5591733f721977cc612cfc1dad7
AbdulRahman Nuzha <abdalrahmannuzha@gmail.com>
2023-09-02 01:18:58 +0300

    import pusher-js and created new Echo instance

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -4,10 +4,10 @@
  * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
 
-import axios from 'axios';
+import axios from "axios";
 window.axios = axios;
 
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
@@ -15,10 +15,10 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
  * allows your team to easily build robust real-time web applications.
  */
 
-// import Echo from 'laravel-echo';
+import Echo from "laravel-echo";
 
-// import Pusher from 'pusher-js';
-// window.Pusher = Pusher;
+import Pusher from "pusher-js";
+window.Pusher = Pusher;
 
 // window.Echo = new Echo({
 //     broadcaster: 'pusher',
@@ -30,3 +30,11 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 //     forceTLS: (import.meta.env.VITE_PUSHER_SCHEME ?? 'https') === 'https',
 //     enabledTransports: ['ws', 'wss'],
 // });
+window.Echo = new Echo({
+    broadcaster: "pusher",
+    key: "a6a823c679db4be8c1ec",
+    cluster: "eu",
+    encrypted: false,
+    authEndpoint: "/broadcasting/auth",
+    forceTLS: true,
+});
