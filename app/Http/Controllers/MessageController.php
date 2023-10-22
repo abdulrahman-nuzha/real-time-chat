a3e28335326f62ad72f61907593c91acebfe3e15
AbdulRahman Nuzha <abdalrahmannuzha@gmail.com>
2023-10-23 01:16:26 +0300

    added message created function

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -6,5 +6,16 @@ use Illuminate\Http\Request;
 
 class MessageController extends Controller
 {
-    //
+    /**
+     * Send new message to the user
+     */
+    public function create(Request $request)
+    {
+        //validate the message
+        //make sure that the room is exist and the users also
+        //send an event
+        //on event Success :
+        //success message
+
+    }
 }
